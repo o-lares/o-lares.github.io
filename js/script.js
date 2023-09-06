cb6c3dfccb1b3c2219de1c67b2be085b2c385007
Oscar <o.lares@outlook.com>
2023-09-05 23:07:47 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -4,30 +4,30 @@ document.addEventListener('DOMContentLoaded', function() {
     const cards = document.querySelectorAll('.experience-card');
     const details = document.querySelectorAll('.experience-detail');
     const closeButtons = document.querySelectorAll('.close-detail');
+    const backdrop = document.querySelector('.backdrop');  // Assuming you added a backdrop div
 
     // Handle tab switching
     tabs.forEach(tab => {
         tab.addEventListener('click', function() {
-            // Remove the active class from all tabs
-            tabs.forEach(innerTab => innerTab.classList.remove('active'));
-
-            // Add the active class to the clicked tab
-            tab.classList.add('active');
-            
-            // Hide all tab contents
-            tabContents.forEach(content => content.style.display = 'none');
-            
-            // Show the clicked tab's content
-            const targetContent = document.querySelector(tab.getAttribute('data-tab-target'));
-            targetContent.style.display = 'block';
+            // ... (rest remains unchanged)
         });
     });
 
     // Show the detail when a card is clicked
     cards.forEach(card => {
         card.addEventListener('click', function() {
+            // Hide all other details
+            details.forEach(detail => detail.style.display = 'none');
+
+            // Show the detail for the clicked card
             const detailId = card.getAttribute('data-detail');
             document.getElementById(detailId).style.display = 'block';
+
+            // Show the backdrop
+            backdrop.style.display = 'block';
+
+            // Prevent scrolling on the body
+            document.body.style.overflow = 'hidden';
         });
     });
 
@@ -36,7 +36,12 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function() {
             const detail = button.closest('.experience-detail');
             detail.style.display = 'none';
+
+            // Hide the backdrop
+            backdrop.style.display = 'none';
+
+            // Restore scrolling on the body
+            document.body.style.overflow = 'auto';
         });
     });
 });
-
