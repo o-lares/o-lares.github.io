4c265f4edbb118bfed65503360d1a6e22e717dd6
Oscar <o.lares@outlook.com>
2023-09-05 23:48:54 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -9,7 +9,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // Handle tab switching
     tabs.forEach(tab => {
         tab.addEventListener('click', function() {
-            // ... (rest remains unchanged)
+        // Remove the active class from all tabs
+        tabs.forEach(innerTab => innerTab.classList.remove('active'));
+
+        // Add the active class to the clicked tab
+        tab.classList.add('active');
+        
+        // Hide all tab contents
+        tabContents.forEach(content => content.style.display = 'none');
+        
+        // Show the clicked tab's content
+        const targetContent = document.querySelector(tab.getAttribute('data-tab-target'));
+        targetContent.style.display = 'block';
         });
     });
 
